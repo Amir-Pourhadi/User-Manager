473a579e980f2498f66960bb6871db0bfe2d0a5f
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-24 23:05:35 +0330

    Create User model

---
+++ server/model/User.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  gender: String,
+  status: String,
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
