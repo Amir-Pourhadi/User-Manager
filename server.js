1a400765cd676ba58a41452dd908733493e12fa8
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-24 10:53:59 +0330

    Get port from dotenv

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
 // Create a new express application instance
 const app = express();
 
+// Load environment variables from .env file
+dotenv.config({ path: "config.env" });
+
 // The port the express app will listen on
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 // Home page route
 app.get("/", (req, res) => {
