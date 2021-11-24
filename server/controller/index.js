252915aa2aa63f39eca97a4d7338bb5c02972731
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-24 23:14:26 +0330

    Create controller functions

---
+++ server/controller/index.js
@@ -0,0 +1,13 @@
+const User = require("../model/User");
+
+// Create and Save a new User
+exports.create = (req, res) => {};
+
+// Retrieve all Users / Retrieve a single User
+exports.find = (req, res) => {};
+
+// Update a User with Id
+exports.update = (req, res) => {};
+
+// Delete a User with Id
+exports.delete = (req, res) => {};
